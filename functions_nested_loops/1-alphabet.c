6129c69230404d4c4ba2cdac2a223ca8b27a532c
Sak1na-IT <pasayevasekine844@gmail.com>
2025-12-03 18:24:52 +0000

    Add 1-alphabet.c task

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,11 +1,16 @@
 #include "main.h"
 
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ *
+ * Return: void
+ */
 void print_alphabet(void)
 {
-char c;
+int i;
 
-for (c = 'a'; c <= 'z'; c++)
-_putchar(c);
+for (i = 'a'; i <= 'z'; i++)
+_putchar(i);
 
 _putchar('\n');
 }
