14e444973061cd9c20f568eb85a66be947160f4b
Sak1na-IT <pasayevasekine844@gmail.com>
2025-12-11 13:48:29 +0000

    python

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,5 +2,10 @@
 #define MAIN_H
 
 void reset_to_98(int *n);
-
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+int _putchar(char c);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
 #endif
