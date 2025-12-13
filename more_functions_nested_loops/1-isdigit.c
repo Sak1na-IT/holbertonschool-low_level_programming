de70180e54e74a0b0a208bca175948a46556abb4
Sak1na-IT <pasayevasekine844@gmail.com>
2025-12-13 20:16:58 +0000

    python

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for uppercase character
+ * @c: the character to check
+ *
+ * Return: 1 if uppercase, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	return (0);
+}
