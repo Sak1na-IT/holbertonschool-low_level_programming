3f8a2fb75e308832ea6c7c3caf95c3dc806f7ad6
Sak1na-IT <pasayevasekine844@gmail.com>
2025-12-13 19:12:07 +0000

    python

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -2,23 +2,21 @@
 
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer
- *
- * Return: void
  */
 void jack_bauer(void)
 {
-int hour, minute;
+	int hour, minute;
 
-for (hour = 0; hour <= 23; hour++)
-{
-for (minute = 0; minute <= 59; minute++)
-{
-_putchar((hour / 10) + '0');
-_putchar((hour % 10) + '0');
-_putchar(':');
-_putchar((minute / 10) + '0');
-_putchar((minute % 10) + '0');
-_putchar('\n');
-}
-}
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar('0' + hour / 10);	/* tens of hour */
+			_putchar('0' + hour % 10);	/* ones of hour */
+			_putchar(':');
+			_putchar('0' + minute / 10);	/* tens of minute */
+			_putchar('0' + minute % 10);	/* ones of minute */
+			_putchar('\n');
+		}
+	}
 }
