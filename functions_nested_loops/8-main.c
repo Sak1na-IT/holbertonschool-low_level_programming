0ca599a6970b77fb91aa9c44d0a76dde0d526800
Sak1na-IT <pasayevasekine844@gmail.com>
2025-12-13 21:17:28 +0000

    python

--- functions_nested_loops/8-main.c
+++ functions_nested_loops/8-main.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - check the code for jack_bauer function
  *
- * Return: Always 0.
+ * Return: Always 0
  */
 int main(void)
 {
-jack_bauer();
-return (0);
+    jack_bauer();  // funksiyanı çağırır
+    return (0);
 }
+
