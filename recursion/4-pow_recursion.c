6daadcceb5c64ec81c012923c567b536cd4e148a
Sak1na-IT <pasayevasekine844@gmail.com>
2025-12-19 13:42:51 +0000

    python

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -2,21 +2,18 @@
 
 /**
  * _pow_recursion - returns the value of x raised to the power of y
- * @x: base
- * @y: exponent
+ * @x: the base
+ * @y: the exponent
  *
- * Return: x raised to the power of y, or -1 if y < 0
- *
- * Description: Uses recursion to compute x^y.
+ * Return: x raised to the power of y
+ *         -1 if y is less than 0
  */
 int _pow_recursion(int x, int y)
 {
-	if(y < 0)
-		return (-1);
-
-	if (y == 0)
-		return (1);
-
-	return (x * _pow_recursion(x, y - 1));
+    if (y < 0)
+        return (-1);
+    if (y == 0)
+        return (1);
+    return (x * _pow_recursion(x, y - 1));
 }
 
