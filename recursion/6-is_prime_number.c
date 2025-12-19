ee4cd1b96c351854c9d2be809d71aa93def7962d
Sak1na-IT <pasayevasekine844@gmail.com>
2025-12-19 14:07:33 +0000

    python

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * is_prime_helper - checks if a number is prime recursively
+ * @n: number to check
+ * @i: current divisor
+ *
+ * Return: 1 if prime, 0 otherwise
+ */
 int is_prime_helper(int n, int i)
 {
 if (n % i == 0)
@@ -9,6 +16,12 @@ return (1);
 return (is_prime_helper(n, i + 1));
 }
 
+/**
+ * is_prime_number - checks if a number is a prime number
+ * @n: number to check
+ *
+ * Return: 1 if prime, 0 otherwise
+ */
 int is_prime_number(int n)
 {
 if (n <= 1)
