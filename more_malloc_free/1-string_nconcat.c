086b735d717d755b5bdb42d11c93d807c7cb9ef0
Sak1na-IT <pasayevasekine844@gmail.com>
2025-12-27 18:03:54 +0000

    python

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* string_nconcat - concatenates s1 and first n bytes of s2
+* @s1: first string
+* @s2: second string
+* @n: number of bytes of s2 to concatenate
+*
+* Return: pointer to new string, or NULL if malloc fails
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+unsigned int i, j, len1 = 0, len2 = 0;
+char *ptr;
+
+if (s1 != NULL)
+{
+while (s1[len1] != '\0')
+len1++;
+}
+if (s2 != NULL)
+{
+while (s2[len2] != '\0')
+len2++;
+}
+if (n >= len2)
+n = len2;
+
+ptr = malloc(len1 + n + 1);
+if (ptr == NULL)
+return (NULL);
+
+for (i = 0; i < len1; i++)
+ptr[i] = s1[i];
+for (j = 0; j < n; j++)
+ptr[i + j] = s2[j];
+ptr[i + j] = '\0';
+
+return (ptr);
+}
+
