1c188087c4a52fb1edf597159e97b2c186ab5db1
Sak1na-IT <pasayevasekine844@gmail.com>
2026-01-10 09:25:29 +0000

    python

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,16 +2,21 @@
 #define DOG_H
 
 /**
- * struct dog - dog structure
+ * struct dog - defines a dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
+ *
+ * Description: structure representing a dog
  */
-struct dog
+typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-};
+} dog_t;
 
-#endif /* DOG_H */
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
