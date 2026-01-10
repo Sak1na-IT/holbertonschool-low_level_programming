e48b72b54441a5b6e1ebdd274014ab5585fc350e
Sak1na-IT <pasayevasekine844@gmail.com>
2026-01-10 09:54:53 +0000

    python

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -12,7 +12,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 int i;
 
 if (array == NULL || cmp == NULL || size <= 0)
-return -1;
+return (-1);
 
 for (i = 0; i < size; i++)
 {
@@ -20,5 +20,5 @@ if (cmp(array[i]))
 return i;
 }
 
-return -1;
+return (-1);
 }
