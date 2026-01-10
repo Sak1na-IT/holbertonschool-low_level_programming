ee1bb3c32bd18d24d87dafedebf2002003a6776e
Sak1na-IT <pasayevasekine844@gmail.com>
2026-01-10 09:40:23 +0000

    python

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,40 +1,79 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <stdlib.h> /* malloc, free, NULL */
 #include "dog.h"
 
+/**
+ * string_len - calculates the length of a string
+ * @s: the string to measure
+ *
+ * Return: the length of the string
+ */
+int string_len(char *s)
+{
+int i = 0;
+
+while (s[i] != '\0')
+i++;
+return (i);
+}
+
+/**
+ * copy_string - allocates memory and copies a string
+ * @s: the string to copy
+ *
+ * Return: pointer to the copied string, or NULL if it fails
+ */
+char *copy_string(char *s)
+{
+int len, i;
+char *copy;
+
+if (s == NULL)
+return (NULL);
+
+len = string_len(s) + 1;
+copy = malloc(len);
+if (copy == NULL)
+return (NULL);
+
+for (i = 0; i < len; i++)
+copy[i] = s[i];
+
+return (copy);
+}
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: pointer to the new dog, NULL if it fails
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *d;
-int len_name, len_owner;
-
-if (name == NULL || owner == NULL)
-return NULL;
 
 d = malloc(sizeof(dog_t));
 if (d == NULL)
-return NULL;
+return (NULL);
 
-len_name = strlen(name) + 1;
-d->name = malloc(len_name);
-if (d->name == NULL)
+d->name = copy_string(name);
+if (name != NULL && d->name == NULL)
 {
 free(d);
-return NULL;
+return (NULL);
 }
-strcpy(d->name, name);
 
-len_owner = strlen(owner) + 1;
-d->owner = malloc(len_owner);
-if (d->owner == NULL)
+d->owner = copy_string(owner);
+if (owner != NULL && d->owner == NULL)
 {
+if (d->name)
 free(d->name);
 free(d);
-return NULL;
+return (NULL);
 }
-strcpy(d->owner, owner);
 
 d->age = age;
 
-return d;
+return (d);
 }
