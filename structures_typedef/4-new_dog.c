1c8b976874144e74524e18cbe50ebf4e3ccea28c
Sak1na-IT <pasayevasekine844@gmail.com>
2026-01-10 09:38:55 +0000

    python

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -3,16 +3,10 @@
 #include <string.h>
 #include "dog.h"
 
-/**
- * new_dog - creates a new dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- * Return: pointer to new dog, NULL if fails
- */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *d;
+int len_name, len_owner;
 
 if (name == NULL || owner == NULL)
 return NULL;
@@ -21,20 +15,24 @@ d = malloc(sizeof(dog_t));
 if (d == NULL)
 return NULL;
 
-d->name = strdup(name);
+len_name = strlen(name) + 1;
+d->name = malloc(len_name);
 if (d->name == NULL)
 {
 free(d);
 return NULL;
 }
+strcpy(d->name, name);
 
-d->owner = strdup(owner);
+len_owner = strlen(owner) + 1;
+d->owner = malloc(len_owner);
 if (d->owner == NULL)
 {
 free(d->name);
 free(d);
 return NULL;
 }
+strcpy(d->owner, owner);
 
 d->age = age;
 
